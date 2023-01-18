53a0deb8f59ee59727d3b582143ba575557e5604
Sachin Kishan <sachinkishan02@gmail.com>
2023-01-18 21:20:54 +0530

    add png image writing

--- IMAGEPROCESSING/IMAGEPROCESSING/IMAGEPROCESSING.cpp
+++ IMAGEPROCESSING/IMAGEPROCESSING/IMAGEPROCESSING.cpp
@@ -3,9 +3,58 @@
 
 #include <iostream>
 
+#include "lodepng.h"
+
+//todo
+/**
+ *  Add image reader for ppm
+ *  Add image reader for png
+ *  Add method to read everything to grayscale
+ *  Add point operations
+ *  
+ *
+ *
+ *  
+ */
+
+std::vector<unsigned char> image; //the raw pixels
+unsigned width, height;
+#pragma region PNG
+void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsigned width, unsigned height) {
+    //Encode the image
+    unsigned const error = lodepng::encode(filename, image, width, height);
+
+    //if there's an error, display it
+    if (error) std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+}
+
+void decodeOneStep(const char* filename) {
+
+    //decode
+    unsigned error = lodepng::decode(image, width, height, filename);
+
+    //if there's an error, display it
+    if (error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+
+    //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
+}
+#pragma endregion
+
 int main()
 {
     std::cout << "Hello World!\n";
+    const char* filename = "out1.png";
+    std::vector<unsigned char> image;
+    const auto aspect_ratio = 1;
+    const size_t image_width = 1024;
+    const size_t image_height = static_cast<int>(image_width / aspect_ratio);
+    int total = image_width * image_height;
+    const int samples_per_pixel = 1;
+    //image resizing
+    image.resize(image_width * image_height * 4);
+
+	encodeOneStep(filename, image, image_width, image_height);
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
