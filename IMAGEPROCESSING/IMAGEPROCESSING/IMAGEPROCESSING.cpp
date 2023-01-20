ccb0a90a4956d64da5ef40dcf12b606f46a5382b
Sachin Kishan <sachinkishan02@gmail.com>
2023-01-20 12:53:09 +0530

    add color in function

--- IMAGEPROCESSING/IMAGEPROCESSING/IMAGEPROCESSING.cpp
+++ IMAGEPROCESSING/IMAGEPROCESSING/IMAGEPROCESSING.cpp
@@ -4,13 +4,12 @@
 #include <iostream>
 
 #include "lodepng.h"
-
+#include "color.h"
 //todo
 /**
- *  Add image reader for ppm
- *  Add image reader for png
  *  Add method to read everything to grayscale
  *  Add point operations
+ *  Add convolutions
  *  
  *
  *
@@ -39,6 +38,34 @@ void decodeOneStep(const char* filename,  std::vector<unsigned char>& image, uns
 #pragma endregion
 
 
+void color_image_pixel(std::vector<unsigned char> &image,int x,int y, color c, unsigned image_width, unsigned image_height)
+{
+
+    //const unsigned char rf = static_cast<unsigned char>(std::min(1., c.r) * 255);
+    //const unsigned char gf = static_cast<unsigned char>(std::min(1., c.g) * 255);
+    //const unsigned char bf = static_cast<unsigned char>(std::min(1., c.b) * 255);
+
+    int val = 4 * image_width * (image_height - y) + 4 * x;
+    //allot color
+    image[val + 0] = c.r;
+    image[val + 1] = c.g;
+    image[val + 2] = c.b;
+    image[val + 3] = 255;//alpha value of 1
+}
+
+//todo: fill in this function such that i can send a x,y position in the image and return the corresponding color value in the form we want
+color return_color_in_pixel()
+{
+    return color();
+}
+
+color color_to_grayscale(color c)
+{
+	//\mathbf{grayscale = 0.3 * R + 0.59 * G + 0.11 * B}
+
+    float col = (0.3 * c.r) + (0.59 * c.g) + (0.11 * c.b);
+}
+
 inline double random_double() {
     // Returns a random real in [0,1).
     return rand() / (RAND_MAX + 1.0);
@@ -48,6 +75,7 @@ inline double random_double(double min, double max) {
     // Returns a random real in [min,max).
     return min + (max - min) * random_double();
 }
+
 
 
 int main()
@@ -60,15 +88,27 @@ int main()
     const auto aspect_ratio = 1;
     unsigned image_width;
 	unsigned image_height;
+
+
     decodeOneStep(inputfilename,inputimage,image_width,image_height);
 	int total = image_width * image_height;
 
-    outputimage = inputimage;
-
-
-
     //image resizing
     outputimage.resize(image_width * image_height * 4);
+
+    //processing
+
+    outputimage = inputimage;
+
+    for(int y=image_height-1;y>0;y--)
+    {
+	    for(int x=0;i<image_height;x++)
+	    {
+            int val = 4 * image_width * (image_height - y) + 4 * x;
+			//todo: allow every pixel to be colored in some color depending on the operation used
+	    }
+    }
+
 
 	encodeOneStep(outputfilename, outputimage, image_width, image_height);
 }
